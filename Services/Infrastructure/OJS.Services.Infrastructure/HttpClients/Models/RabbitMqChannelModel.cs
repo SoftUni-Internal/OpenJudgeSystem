4c34e0aceb3b06cb1cf6b44732cb63cede6052b8
Georgi Georgiev <gogo4ds@gmail.com>
2025-03-05 13:39:43 +0200

    Docs

--- Services/Infrastructure/OJS.Services.Infrastructure/HttpClients/Models/RabbitMqChannelModel.cs
+++ Services/Infrastructure/OJS.Services.Infrastructure/HttpClients/Models/RabbitMqChannelModel.cs
@@ -7,12 +7,21 @@ public class RabbitMqChannelModel
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Number of messages that are currently being processed by consumers.
+    /// </summary>
     [JsonPropertyName("messages_unacknowledged")]
     public int MessagesUnacknowledged { get; set; }
 
+    /// <summary>
+    /// How many message is the channel allowed to prefetch.
+    /// </summary>
     [JsonPropertyName("prefetch_count")]
     public int PrefetchCount { get; set; }
 
+    /// <summary>
+    /// Number of consumers that are currently connected to the channel.
+    /// </summary>
     [JsonPropertyName("consumer_count")]
     public int ConsumerCount { get; set; }
 }
--- Services/Infrastructure/OJS.Services.Infrastructure/HttpClients/Models/RabbitMqConsumerModel.cs
+++ Services/Infrastructure/OJS.Services.Infrastructure/HttpClients/Models/RabbitMqConsumerModel.cs
@@ -4,9 +4,15 @@ using System.Text.Json.Serialization;
 
 public class RabbitMqConsumerModel
 {
+    /// <summary>
+    /// Is the consumer active. If the consumer is not active, it will not receive any messages.
+    /// </summary>
     [JsonPropertyName("active")]
     public bool Active { get; set; }
 
+    /// <summary>
+    /// The number of messages that the consumer will prefetch.
+    /// </summary>
     [JsonPropertyName("prefetch_count")]
     public int PrefetchCount { get; set; }
 
