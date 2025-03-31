e8e74808d72d84b4df6446c2c90c6b0dbda46c99
Georgi Georgiev <gogo4ds@gmail.com>
2025-03-31 16:57:31 +0300

    Added redirect to /login in /profile

--- Servers/UI/OJS.Servers.Ui/ClientApp/tests/example.spec.ts
+++ Servers/UI/OJS.Servers.Ui/ClientApp/tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { expect,test } from './fixtures/auth.fixture';
+import { expect, test } from './fixtures/auth.fixture';
 
 test('has title', async ({ page }) => {
     await page.goto('/') ;
@@ -12,4 +12,19 @@ test('can log in', async ({ page, auth }) => {
     const response = await page.goto('/profile');
     expect(response?.status()).toBe(200);
     await expect(page).toHaveURL('/profile');
+});
+
+test('can log out', async ({ page, auth }) => {
+    await auth.loginDefaultTestUser();
+    await page.goto('/profile');
+    await expect(page).toHaveURL('/profile');
+
+    await page.goto('/logout');
+
+    await expect(page.getByText('You are now successfully logged out')).toBeVisible();
+    await page.waitForURL('/');
+    await expect(page).toHaveURL('/');
+
+    const response = await page.goto('/profile');
+    expect(response?.status()).not.toBe(200);
 });
