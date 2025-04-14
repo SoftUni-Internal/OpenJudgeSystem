fb026adda9534ac18bb4433faa9ebc066d550475
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2025-04-14 12:51:51 +0300

    1675 improve worker testing with testcontainers (#1584)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1675
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1676
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1677
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1678

---
+++ Tests/IntegrationTests/IntegrationTests.Worker/BaseSubmissionFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace IntegrationTests.Worker;
+
+using OJS.PubSub.Worker.Models.Submissions;
+
+public abstract class BaseSubmissionFactory<TParams>
+{
+    private int id;
+
+    /// <summary>
+    /// A method for getting a submission for test execution.
+    /// <param name="strategyParameters">These are the unique properties the strategy's test requires to be changed in order to get the expected result.</param>
+    /// </summary>
+    public abstract SubmissionForProcessingPubSubModel GetSubmission(TParams strategyParameters);
+
+    /// <summary>
+    /// A thread-safe way for creating a unique id for each submission. Based on the 'fixture' implementation, the IDs MUST be unique.
+    /// This is faster than using 'lock'.
+    /// </summary>
+    protected int GetNextId()
+        => Interlocked.Increment(ref this.id);
+}
