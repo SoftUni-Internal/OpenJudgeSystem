dd6738fb2aacba2b44135c87df8b978669183cb2
Georgi Georgiev <gogo4ds@gmail.com>
2025-05-23 11:20:12 +0300

    progress

--- Servers/Infrastructure/OJS.Servers.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
+++ Servers/Infrastructure/OJS.Servers.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -3,90 +3,39 @@ namespace OJS.Servers.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OJS.Services.Infrastructure.Configurations;
-using OpenTelemetry.Exporter;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-using Serilog;
-using Serilog.Sinks.OpenTelemetry;
-using System;
-using System.Collections.Generic;
 
 public static class WebApplicationBuilderExtensions
 {
     public static WebApplicationBuilder ConfigureOpenTelemetry(this WebApplicationBuilder builder)
     {
-        var environment = builder.Environment;
-        var environmentName = environment.EnvironmentName.ToLower();
-        var applicationName = environment.GetShortApplicationName().ToLower();
-        var otlpCollectorBaseUrl = builder.Configuration.GetSectionWithValidation<ApplicationConfig>().OtlpCollectorBaseUrl;
-        var otlpCollectorEndpoint = otlpCollectorBaseUrl != null
-            ? new Uri(otlpCollectorBaseUrl)
-            : null;
-        var hostName = Environment.GetEnvironmentVariable(Workers.Common.Constants.HostIpEnvironmentVariable)
-                       ?? Environment.MachineName;
+        var applicationName = builder.Environment.GetShortApplicationName().ToLower();
 
-        builder.Host.UseSerilog((hostingContext, configuration) =>
+        builder.Logging.AddOpenTelemetry(loggingBuilder =>
         {
-            configuration
-                .ReadFrom.Configuration(hostingContext.Configuration)
-                .Enrich.FromLogContext()
-                .WriteTo.Async(wt => wt.Console())
-                .WriteTo.OpenTelemetry(options =>
-                {
-                    options.Endpoint = otlpCollectorBaseUrl;
-                    options.Protocol = OtlpProtocol.Grpc;
-                    options.ResourceAttributes = new Dictionary<string, object>
-                    {
-                        ["service.name"] = applicationName,
-                        ["deployment.environment"] = environmentName,
-                        ["host.name"] = hostName,
-                    };
-                });
+            loggingBuilder.IncludeFormattedMessage = true;
+            loggingBuilder.IncludeScopes = true;
         });
 
         var otel = builder.Services.AddOpenTelemetry();
 
-        otel.ConfigureResource(resource => resource
-            .AddService(serviceName: applicationName)
-            .AddTelemetrySdk()
-            .AddAttributes(new Dictionary<string, object>
-            {
-                ["deployment.environment.name"] = environmentName,
-                ["host.name"] = hostName,
-            }));
+        otel.ConfigureResource(resource => resource.AddService(applicationName));
 
         otel.WithMetrics(metrics =>
         {
             metrics
                 .AddHttpClientInstrumentation()
                 .AddAspNetCoreInstrumentation()
-                .AddRuntimeInstrumentation()
-                .AddMeter("Microsoft.AspNetCore.Hosting")
-                .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
-                .AddMeter("System.Net.Http")
-                .AddMeter("System.Net.NameResolution");
-
-            metrics.AddConsoleExporter();
-
-            if (otlpCollectorEndpoint != null)
-            {
-                metrics.AddOtlpExporter(options =>
-                {
-                    options.Endpoint = otlpCollectorEndpoint;
-                    options.Protocol = OtlpExportProtocol.Grpc;
-                });
-            }
-            else
-            {
-                metrics.AddConsoleExporter();
-            }
+                .AddRuntimeInstrumentation();
         });
 
         otel.WithTracing(tracing =>
         {
-            if (environment.IsDevelopment())
+            if (builder.Environment.IsDevelopment())
             {
                 // In development, always sample traces, so we can see them in the console
                 tracing.SetSampler<AlwaysOnSampler>();
@@ -96,22 +45,13 @@ public static class WebApplicationBuilderExtensions
                 .AddHttpClientInstrumentation()
                 .AddAspNetCoreInstrumentation()
                 .AddEntityFrameworkCoreInstrumentation();
-
-            tracing.AddConsoleExporter();
-
-            if (otlpCollectorEndpoint != null)
-            {
-                tracing.AddOtlpExporter(options =>
-                {
-                    options.Endpoint = otlpCollectorEndpoint;
-                    options.Protocol = OtlpExportProtocol.Grpc;
-                });
-            }
-            else
-            {
-                tracing.AddConsoleExporter();
-            }
         });
+
+        var otlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+        if (otlpEndpoint != null)
+        {
+            otel.UseOtlpExporter();
+        }
 
         return builder;
     }
