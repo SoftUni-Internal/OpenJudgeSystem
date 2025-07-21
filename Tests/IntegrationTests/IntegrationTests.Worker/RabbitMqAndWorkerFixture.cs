dfe851e7f83cd5d2f054a33dbc9943c154a1134f
Georgi Georgiev <gogo4ds@gmail.com>
2025-07-21 18:13:48 +0300

    Revert

--- Tests/IntegrationTests/IntegrationTests.Worker/RabbitMqAndWorkerFixture.cs
+++ Tests/IntegrationTests/IntegrationTests.Worker/RabbitMqAndWorkerFixture.cs
@@ -94,7 +94,7 @@ public class RabbitMqAndWorkerFixture : IAsyncLifetime
             .WithHostname(WorkerName)
             .WithEnvironment("MessageQueue__Host", this.rabbitMqContainer.IpAddress)
             .WithEnvironment("DOTNET_ENVIRONMENT", "Production")
-            .WithBindMount(dockerCompatiblePath, "/app/ExecutionStrategies")
+            .WithBindMount(dockerCompatiblePath, "/tmp/ExecutionStrategies")
             .WithBindMount("/var/run/docker.sock", "/var/run/docker.sock")
             .DependsOn(this.rabbitMqContainer)
             .WithLogger(loggerFactory.CreateLogger<ImageFromDockerfileBuilder>())
